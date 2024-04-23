8ca82c266283d519c7a63010d61dfb16cee74888
rishavmanori <55047639+rishavmanori@users.noreply.github.com>
2024-04-23 08:50:34 +0000

    Update website

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let currFolder;
 async function getSongs(folder) {
   currFolder = folder;
   let a = await fetch(`${currFolder}`);
+  console.log(currFolder);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
